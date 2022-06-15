fb35f738cb31366ed3698bbbf447d75c7def5aaf
Silvia Trejo <s.y.trejo26@gmail.com>
2022-06-14 22:51:57 -0400

    First commit...working with dependencies

---
+++ DependenciesDemo/src/association/Classroom.java
@@ -0,0 +1,8 @@
+package association;
+
+public class Classroom {
+	
+	//HAS-A relationship - the dependency is a field in the dependent class
+	private Trainer trainer;
+
+}
---
+++ DependenciesDemo/src/dependencies/Trainee.java
@@ -0,0 +1,12 @@
+package dependencies;
+
+public class Trainee {
+	
+	//Uses-A relationship
+	//option A: a method takes in the dependency as an arg;
+	//option B: returns it as a return value
+	public int takeExam(Exam exam) {
+		return 0;
+	}
+
+}
